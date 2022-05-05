8ffe9c6962857c12766b846fd7e8f20699e5b817
tomek <tomaszpawelkowalski@gmail.com>
2022-05-05 19:13:07 +0200

    test: set more tests for method ones

--- test/Yatzy.test.ts
+++ test/Yatzy.test.ts
@@ -16,11 +16,13 @@ describe('Yatzy', () => {
 });
 
 describe('Ones', () => {
+  it('score the sum of 1s', () => {
+		const game = new Yatzy(1, 2, 2, 2, 2);
+		assert.strictEqual(1, game.ones());
+	});
 	it('score the sum of 1s', () => {
-		assert.strictEqual(1, Yatzy.ones(1, 2, 3, 4, 5));
-		assert.strictEqual(2, Yatzy.ones(1, 2, 1, 4, 5));
-		assert.strictEqual(0, Yatzy.ones(6, 2, 2, 4, 5));
-		assert.strictEqual(4, Yatzy.ones(1, 2, 1, 1, 1));
+		const game = new Yatzy(1, 1, 2, 2, 2);
+		assert.strictEqual(2, game.ones());
 	});
 });
 
