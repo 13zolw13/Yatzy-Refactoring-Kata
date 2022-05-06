efaa98638f43f6301e799b5aca75148bc4720081
tomek <tomaszpawelkowalski@gmail.com>
2022-05-06 17:20:38 +0200

    refactor: move methods

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -74,6 +74,17 @@ export default class Yatzy {
 		if (n == 2) return score * 2;
 		else return 0;
 	}
+	static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+		var t;
+		t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+		t[d1 - 1]++;
+		t[d2 - 1]++;
+		t[d3 - 1]++;
+		t[d4 - 1]++;
+		t[d5 - 1]++;
+		for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
+		return 0;
+	}
 
 	static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
 		var tallies;
@@ -87,17 +98,6 @@ export default class Yatzy {
 		return 0;
 	}
 
-	static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-		var t;
-		t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-		t[d1 - 1]++;
-		t[d2 - 1]++;
-		t[d3 - 1]++;
-		t[d4 - 1]++;
-		t[d5 - 1]++;
-		for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-		return 0;
-	}
 
 	static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
 		var tallies;
