5b2ac1987a33be046c787fdd45e60d26a651e985
tomek <tomaszpawelkowalski@gmail.com>
2022-05-07 17:29:01 +0200

    reafactor: small Straight method

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -90,17 +90,10 @@ export default class Yatzy {
 		return this.findSameScoreReturnHighestValue(counts, 4, false);
 	}
 
-	static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-		var tallies;
-		tallies = [0, 0, 0, 0, 0, 0, 0];
-		tallies[d1 - 1] += 1;
-		tallies[d2 - 1] += 1;
-		tallies[d3 - 1] += 1;
-		tallies[d4 - 1] += 1;
-		tallies[d5 - 1] += 1;
-		if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1)
-			return 15;
-		return 0;
+	smallStraight(): number {
+		const patternSmallStraight = '12345';
+		const smallStraightReturnValue = this.dice.sort((a, b) => a - b).join('');
+		return smallStraightReturnValue === patternSmallStraight ? 15 : 0;
 	}
 
 	static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
