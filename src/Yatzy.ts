06e2fb6f9d21abfb6fec24dd93b6297f811d35af
tomek <tomaszpawelkowalski@gmail.com>
2022-05-07 18:25:29 +0200

    refactor: edit fullhouse method

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -100,38 +100,12 @@ export default class Yatzy {
 		const patternLargeStraight = '23456';
 		const largeStraightReturnValue = this.dice.sort((a, b) => a - b).join('');
 		return largeStraightReturnValue === patternLargeStraight ? 20 : 0;
-	
 	}
 
-	static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-		var tallies;
-		var _2 = false;
-		var i;
-		var _2_at = 0;
-		var _3 = false;
-		var _3_at = 0;
-
-		tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-		tallies[d1 - 1] += 1;
-		tallies[d2 - 1] += 1;
-		tallies[d3 - 1] += 1;
-		tallies[d4 - 1] += 1;
-		tallies[d5 - 1] += 1;
-
-		for (i = 0; i != 6; i += 1)
-			if (tallies[i] == 2) {
-				_2 = true;
-				_2_at = i + 1;
-			}
-
-		for (i = 0; i != 6; i += 1)
-			if (tallies[i] == 3) {
-				_3 = true;
-				_3_at = i + 1;
-			}
-
-		if (_2 && _3) return _2_at * 2 + _3_at * 3;
-		else return 0;
+	fullHouse(): number {
+		const counts: number[] = this.getSameDices();
+		const returnValue = counts.filter((a) => a !== 0 && (a === 2 || a === 3)).length === 2 ? 25 : 0;
+		return returnValue as number;
 	}
 }
 
