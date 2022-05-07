c5394755bea514675d325f4c3f69fa925aff1c8e
tomek <tomaszpawelkowalski@gmail.com>
2022-05-07 20:37:57 +0200

    test: more cases for small straight

--- test/Yatzy.test.ts
+++ test/Yatzy.test.ts
@@ -183,10 +183,18 @@ describe(Yatzy.name, () => {
 			const game = new Yatzy(1, 2, 3, 4, 5);
 			assert.strictEqual(15, game.smallStraight());
 		});
-			it('edge case should return 0', () => {
-				const game = new Yatzy(2, 3, 4, 5, 6);
-				assert.strictEqual(0, game.smallStraight());
-			});
+		it('edge case should return 0', () => {
+			const game = new Yatzy(2, 3, 4, 5, 6);
+			assert.strictEqual(0, game.smallStraight());
+		});
+		it('edge case should return 0', () => {
+			const game = new Yatzy(2, 3, 4, 1, 6);
+			assert.strictEqual(0, game.smallStraight());
+		});
+		it('edge case should return 0', () => {
+			const game = new Yatzy(2, 3, 4, 1, 5);
+			assert.strictEqual(15, game.smallStraight());
+		});
 	});
 
 	describe('Large straight', () => {
