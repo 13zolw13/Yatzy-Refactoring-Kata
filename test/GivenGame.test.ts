4d43cd3532624e7cbd0a37a0e4b70a9bc23b0274
tomek <tomaszpawelkowalski@gmail.com>
2022-05-09 22:09:17 +0200

    test: separate cases for create round

--- test/GivenGame.test.ts
+++ test/GivenGame.test.ts
@@ -3,6 +3,18 @@ import { GivenGame, Round } from '../src/GivenGame';
 import Yatzy from '../src/Yatzy';
 
 describe(GivenGame.name, () => {
+
+	it('should correctly add one game ', () => {
+		const game = new Yatzy(1, 2, 3, 4, 5);
+		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
+		const givenGame = new GivenGame();
+		const newRound = new Round(4, 'fours');
+		givenGame.addRound(newRound);
+
+		assert.strictEqual(1, givenGame.getRounds.length);
+		assert.strictEqual(4, givenGame.getRounds[0].score);
+		assert.strictEqual('fours', givenGame.getRounds[0].category);
+	});
 	it('should  add one game - cannot add second roll with the same category', () => {
 		const game = new Yatzy(1, 2, 3, 4, 5);
 		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
@@ -12,12 +24,11 @@ describe(GivenGame.name, () => {
 
 		givenGame.addRound(newRound);
 		givenGame.addRound(secondRound);
-		console.log(givenGame.getRounds);
+
 		assert.strictEqual(1, givenGame.getRounds.length);
 		assert.strictEqual(4, givenGame.getRounds[0].score);
 		assert.strictEqual('fours', givenGame.getRounds[0].category);
 	});
-
 	it('should correctly add two rounds ', () => {
 		const game = new Yatzy(1, 2, 3, 4, 5);
 		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
