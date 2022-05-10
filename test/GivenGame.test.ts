bcbfec614719b83632f1b387512d1aad17355140
tomek <tomaszpawelkowalski@gmail.com>
2022-05-10 14:22:55 +0200

    test: reorgenize cases

--- test/GivenGame.test.ts
+++ test/GivenGame.test.ts
@@ -5,56 +5,60 @@ import Yatzy from '../src/Yatzy';
 
 describe(GivenGame.name, () => {
 
-	it('should correctly add one game ', () => {
-		const game = new Yatzy(1, 2, 3, 4, 5);
-		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
-		const givenGame = new GivenGame();
-		const newRound = new Round(4, 'fours');
-		givenGame.addRound(newRound);
+	describe('Add only one new round', () => {
+		it('should correctly add one game ', () => {
+			const game = new Yatzy(1, 2, 3, 4, 5);
+			assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
+			const givenGame = new GivenGame();
+			const newRound = new Round(4, 'fours');
+			givenGame.addRound(newRound);
 
-		assert.strictEqual(1, givenGame.getRounds.length);
-		assert.strictEqual(4, givenGame.getRounds[0].score);
-		assert.strictEqual('fours', givenGame.getRounds[0].category);
+			assert.strictEqual(1, givenGame.getRounds.length);
+			assert.strictEqual(4, givenGame.getRounds[0].score);
+			assert.strictEqual('fours', givenGame.getRounds[0].category);
+		});
 	});
-	it('should  add one game - cannot add second roll with the same category', () => {
-		const game = new Yatzy(1, 2, 3, 4, 5);
-		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
-		const givenGame = new GivenGame();
-		const secondRound = new Round(4, 'fours');
-		const newRound = new Round(4, 'fours');
+	describe('Add two rounds', () => {
+		it('should  add one game - cannot add second roll with the same category', () => {
+			const game = new Yatzy(1, 2, 3, 4, 5);
+			assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
+			const givenGame = new GivenGame();
+			const secondRound = new Round(4, 'fours');
+			const newRound = new Round(4, 'fours');
 
-		givenGame.addRound(newRound);
-		givenGame.addRound(secondRound);
+			givenGame.addRound(newRound);
+			givenGame.addRound(secondRound);
 
-		assert.strictEqual(1, givenGame.getRounds.length);
-		assert.strictEqual(4, givenGame.getRounds[0].score);
-		assert.strictEqual('fours', givenGame.getRounds[0].category);
+			assert.strictEqual(1, givenGame.getRounds.length);
+			assert.strictEqual(4, givenGame.getRounds[0].score);
+			assert.strictEqual('fours', givenGame.getRounds[0].category);
+		});
+
+		it('should correctly add two rounds ', () => {
+			const game = new Yatzy(1, 2, 3, 4, 5);
+			assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
+			const givenGame = new GivenGame();
+			const secondRound = new Round(4, 'fours');
+			const newRound = new Round(6, 'fives');
+
+			givenGame.addRound(newRound);
+			givenGame.addRound(secondRound);
+			assert.strictEqual(2, givenGame.getRounds.length);
+			assert.strictEqual(10, givenGame.totalRounds);
+		});
 	});
-	it('should correctly add two rounds ', () => {
-		const game = new Yatzy(1, 2, 3, 4, 5);
-		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
-		const givenGame = new GivenGame();
-		const secondRound = new Round(4, 'fours');
-		const newRound = new Round(6, 'fives');
+	describe('Given method', () => {
+		it('should  return one less category to play', () => {
+			const game = new Yatzy(1, 2, 3, 4, 5);
+			assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
+			const givenGame = new GivenGame();
+			const secondRound = new Round(4, 'fours');
 
-		givenGame.addRound(newRound);
-		givenGame.addRound(secondRound);
-		assert.strictEqual(2, givenGame.getRounds.length);
-		assert.strictEqual(10, givenGame.totalRounds);
+			const newGiven = givenGame.given(secondRound);
+			assert.strictEqual(1, givenGame.getRounds.length);
+			assert.strictEqual(4, givenGame.getRounds[0].score);
+			assert.strictEqual('fours', givenGame.getRounds[0].category);
+			assert.strictEqual(12, newGiven.length);
+		});
 	});
-
-	
-	it('should  return one less category to play', () => {
-		const game = new Yatzy(1, 2, 3, 4, 5);
-		assert.strictEqual(4, game.OnesTwosTreesFourthsFivesSixes('fours'));
-		const givenGame = new GivenGame();
-		const secondRound = new Round(4, 'fours');
-
-		const newGiven = givenGame.given(secondRound);
-		assert.strictEqual(1, givenGame.getRounds.length);
-		assert.strictEqual(4, givenGame.getRounds[0].score);
-		assert.strictEqual('fours', givenGame.getRounds[0].category);
-		assert.strictEqual(12, newGiven.length);
-	});
-
 });
