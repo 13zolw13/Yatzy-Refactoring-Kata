679526a2a8c8e634687abcd749301ba95e3002a3
tomek <tomaszpawelkowalski@gmail.com>
2022-05-11 12:26:11 +0200

    refactor: SmallStraight/LargeStraight to one liner

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -5,16 +5,14 @@ export default class Yatzy extends YatzyThrow {}
 export class SmallStraight extends YatzyThrow {
 	private smallStraightString = '12345';
 	score(): number {
-		const smallStraightReturnValue = this.dice.sort((a, b) => a - b).join('');
-		return smallStraightReturnValue === this.smallStraightString ? 15 : 0;
+		return this.dice.sort((a, b) => a - b).join('') === this.smallStraightString ? 15 : 0;
 	}
 }
 
 export class LargeStraight extends YatzyThrow {
 	private largeStraightString = '23456';
 	score(): number {
-		const largeStraightReturnValue = this.dice.sort((a, b) => a - b).join('');
-		return largeStraightReturnValue === this.largeStraightString ? 20 : 0;
+		return this.dice.sort((a, b) => a - b).join('') === this.largeStraightString ? 20 : 0;
 	}
 }
 
